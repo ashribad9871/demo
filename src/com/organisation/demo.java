331ee1e0766f5521187678c29e2f14270de95277
Ashribad9871 <ashi19996@gmail.com>
2022-06-28 20:13:33 +0530

    added:demo file

---
+++ src/com/organisation/demo.java
@@ -0,0 +1,13 @@
+package com.organisation;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        // demo dm = new demo ();
+        System.out.println("results="+addition(10,35));
+    }
+    static int addition (int x,int y){
+        return x + y;
+
+    }
+}
