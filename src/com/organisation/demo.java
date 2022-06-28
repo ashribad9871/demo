546dd58c4203409c076e6611357f11a790ded8ed
Ashribad9871 <ashi19996@gmail.com>
2022-06-28 20:45:13 +0530

    refractor subtraction method

--- src/com/organisation/demo.java
+++ src/com/organisation/demo.java
@@ -5,9 +5,12 @@ public class demo {
         System.out.println("hello world");
         // demo dm = new demo ();
         System.out.println("results="+addition(10,35));
+        System.out.println("results="+subtraction(52,35));
     }
     static int addition (int x,int y){
         return x + y;
-
+    }
+    static int subtraction (int x,int y) {
+        return x - y;
     }
 }
